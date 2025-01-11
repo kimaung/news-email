5a74ee36beb0035c56c88f9c3b5b0520287fa62f
Aes Saputra <aessaputra@yahoo.com>
2025-01-11 22:22:11 +0700

    News Email

--- send_news_email.py
+++ send_news_email.py
@@ -3,20 +3,21 @@ import smtplib
 from email.mime.text import MIMEText  
 from email.mime.multipart import MIMEMultipart  
 import logging  
+import os  
   
 # Konfigurasi logging  
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')  
   
 # API Details  
 API_URL = "https://api.currentsapi.services/v1/latest-news"  
-API_KEY = "${{ secrets.CURRENTSAPI_KEY }}"  
+API_KEY = os.getenv('CURRENTSAPI_KEY')  
 LANGUAGE = "en"  
   
 # Email Details  
-SMTP_SERVER = "smtp.gmail.com"  
-SMTP_PORT = 465  
-EMAIL_ADDRESS = "${{ secrets.MAIL_USERNAME }}"  
-EMAIL_PASSWORD = "${{ secrets.MAIL_PASSWORD }}"  
+SMTP_SERVER = "smtp.mail.yahoo.com"  
+SMTP_PORT = 587  
+EMAIL_ADDRESS = os.getenv('MAIL_USERNAME')  
+EMAIL_PASSWORD = os.getenv('MAIL_PASSWORD')  
 RECIPIENT_EMAIL = "aessaputra@yahoo.com"  
   
 def fetch_news():  
